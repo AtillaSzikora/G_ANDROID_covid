51a98dcee7fa9e9c4b7d1a2e858645b5a4ba0c7b
AtillaSzikora <szikora.atilla@gmail.com>
2016-07-23 18:56:35 +0200

    Some fake bugfxin. :/

--- app/src/main/java/com/szikora/covid/MainActivity.java
+++ app/src/main/java/com/szikora/covid/MainActivity.java
@@ -17,66 +17,87 @@ public class MainActivity extends AppCompatActivity {
     private Button signUpButton;
     private SharedPreferences mPrefs;
     private boolean isEmailExist;
+    private String userJson;
     private Gson gson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initialization(); }
+        initialization();
+    }
 
     private void initialization() {
         email = (EditText) findViewById(R.id.emailField);
         password = (EditText) findViewById(R.id.passwordField);
-        logInButton= (Button) findViewById(R.id.logInButton);
-        signUpButton= (Button) findViewById(R.id.signUpButton);
+        logInButton = (Button) findViewById(R.id.logInButton);
+        signUpButton = (Button) findViewById(R.id.signUpButton);
         mPrefs = getPreferences(MODE_PRIVATE);
         gson = new Gson();
         logInButtonClick();
-        signUpButtonClick(); }
+        signUpButtonClick();
+    }
 
     private void logInButtonClick() {
         logInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (isAccountExist()) {
-                    Toast.makeText(getApplicationContext(), "Successfully logged in!", Toast.LENGTH_LONG).show(); }
-                else { Toast.makeText(getApplicationContext(), "Wrong email or password!", Toast.LENGTH_LONG).show(); }
-            } }); }
+                    Toast.makeText(getApplicationContext(), "Successfully logged in!", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Wrong email or password!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
 
     private void signUpButtonClick() {
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!isAccountExist() && !isEmailExist) { validateInputs();
-                } else {
-                    Toast.makeText(getApplicationContext(), "User already exists!", Toast.LENGTH_SHORT).show(); }
-            } }); }
+                if (areInputsValid()) {
+                    if (isAccountExist() || isEmailExist) {
+                        Toast.makeText(getApplicationContext(), "User already exists!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        signUpUser(new User(email.getText().toString(), password.getText().toString()));
+                        Toast.makeText(getApplicationContext(), email.getText().toString() + " successfully signed up!",
+                                Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+    }
 
-    private void validateInputs() {
+    private boolean areInputsValid() {
         if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email.getText().toString()).matches()) {
             Toast.makeText(getApplicationContext(), "Wrong email format!", Toast.LENGTH_LONG).show();
-        } else if (
-            password.getText().toString().length() < 4) {
+            return false;
+        } else if (password.getText().toString().length() < 4) {
             Toast.makeText(getApplicationContext(), "Too short password!", Toast.LENGTH_LONG).show();
-        } else {
-            putUserToSharedPref(new User(email.getText().toString(), password.getText().toString()));
-            Toast.makeText(getApplicationContext(), email.getText().toString() + " successfully signed up!", Toast.LENGTH_LONG).show(); } }
-
-    private void putUserToSharedPref(User user) {
-        SharedPreferences.Editor prefsEditor = mPrefs.edit();
-        String userJson = gson.toJson(user);
-        prefsEditor.putString(user.getEmail(), userJson);
-        prefsEditor.apply(); }
+            return false;
+        }
+        return true;
+    }
 
     private boolean isAccountExist() {
-        Map<String,?> usersMap = mPrefs.getAll();
-        for(Map.Entry<String,?> entry : usersMap.entrySet()){
-            String userJson = (String) entry.getValue();
-            User user = gson.fromJson(userJson, User.class);
+        User user;
+        Map<String, ?> usersMap = mPrefs.getAll();
+        for (Map.Entry<String, ?> entry : usersMap.entrySet()) {
+            userJson = (String) entry.getValue();
+            user = gson.fromJson(userJson, User.class);
             isEmailExist = user.getEmail().equals(email.getText().toString());
-            if(user.getEmail().equals(email.getText().toString())
-            && user.getPassword().equals(password.getText().toString())){
-                return true; } }
-        return false; }
+            if (user.getEmail().equals(email.getText().toString())
+                    && user.getPassword().equals(password.getText().toString())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void signUpUser(User user) {
+        SharedPreferences.Editor prefsEditor = mPrefs.edit();
+        userJson = gson.toJson(user);
+        prefsEditor.putString(user.getEmail(), userJson);
+        prefsEditor.apply();
+    }
 }
